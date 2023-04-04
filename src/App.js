42d9d834952f95a54897e3566d30384c2ada1ea9
shema <shemaserge@gmail.com>
2023-04-04 09:09:14 +0200

    Mud-Login-btn-updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Footer from './Components/Footer/footer';
 import Mud_Login from './Components/Mudugudu/mud_login';
+import FormWithPhoneNumber from './phonenumber';
 import './App.css';
 
 function App() {
   return (
     <div>
       {/* <Footer/> */}
-      <Mud_Login/>
+      <Mud_Login />
     </div>
   );
 }
