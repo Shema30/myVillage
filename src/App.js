93e66945e556018c68b434ba9ae707dbbd3adc0f
shema <shemaserge@gmail.com>
2023-04-04 10:18:44 +0200

    Mudugudu login Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Footer from './Components/Footer/footer';
 import Mud_Login from './Components/Mudugudu/mud_login';
-import FormWithPhoneNumber from './phonenumber';
 import './App.css';
 
 function App() {
--- src/Components/Mudugudu/mud_login.jsx
+++ src/Components/Mudugudu/mud_login.jsx
@@ -4,49 +4,51 @@ import './mud_log.css'
 class Mud_Login extends Component {
     render() {
         return (
-            <div className='mud-login-page'>
+            <>
+                <div className='mud-login-page'>
 
-                <div className='left-side'>
+                    <div className='left-side'>
 
-                    <div className='top'>
-                        <button type="button" className="btn btn-secondary">Back</button>
-                        <h1>Welcome  !</h1>
+                        <div className='top'>
+                            <button type="button" className="btn btn-secondary">Back</button>
+                            <h1>Welcome  !</h1>
+                        </div>
+
+                        <div className='logo'>
+                            <h1>My Village</h1>
+                        </div>
+
+                        <div className='bottom'>
+                            <h4>“Keeping you connected to your village”</h4>
+                        </div>
+
                     </div>
 
-                    <div className='logo'>
-                        <h1>My Village</h1>
-                    </div>
+                    <div className='right-side'>
+                        <div className='mud-login'>
+                            <h2>Mudugudu Login</h2>
 
-                    <div className='bottom'>
-                        <h4>“Keeping you connected to your village”</h4>
+                            <form>
+
+                                <div className="container">
+                                    <label for="telnum"><b>Telphone Number</b></label>
+                                    <input type="tel" placeholder="+250" name="telnum" />
+
+                                    <label for="psw"><b>Password</b></label>
+                                    <input type="password" placeholder="Enter Password" name="psw" />
+                                </div>
+
+                                <div className="log-btn">
+                                    <button type="submit">Login</button>
+                                </div>
+
+                            </form>
+
+                        </div>
                     </div>
 
                 </div>
-
-                <div className='right-side'>
-                    <div className='mud-login'>
-                        <h2>Mudugudu Login</h2>
-
-                        <form>
-
-                            <div className="container">
-                                <label for="uname"><b>Telphone Number</b></label>
-                                <input type="text" placeholder="+250" name="uname" />
-
-                                <label for="psw"><b>Password</b></label>
-                                <input type="password" placeholder="Enter Password" name="psw" />
-                            </div>
-
-                            <div className="log-btn">
-                            <button type="submit">Login</button>
-                            </div>
-                            
-                        </form>
-
-                    </div>
-                </div>
-
-            </div>
+            </>
         );
     }
 }
